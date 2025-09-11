4eb135cec4dde3f126d924fbe35106c6b3a4c316
dvtuong <dangvinhtuong12@gmail.com>
2025-09-11 23:03:04 +0700

    make server single thread

--- wmux.c
+++ wmux.c
@@ -16,18 +16,13 @@ static const char *SERVER_STDIN_PIPE_NAME = "\\\\.\\pipe\\server_stdin";
 static const char *SERVER_STDOUT_PIPE_NAME = "\\\\.\\pipe\\server_stdout";
 static const char *THREAD_OUTPUT_PIPE_NAME = "\\\\.\\pipe\\thread_stdout";
 
-typedef struct {
-    HANDLE thread_out;
-    HANDLE thread_in;
-} ProcessEventHandlerArg;
-
 #define STDOUT_INDEX 0
 #define PROCESS_INDEX 1
 #define PROCESS_THREAD_INDEX 2
 
 #define SERVER_INPUT_INDEX 0
-#define PROCESS_HANDLER_THREAD_INDEX 1
-#define THREAD_OUTPUT_INDEX 2
+#define PROCESS_HANDLE_INDEX 1
+#define CONSOLE_OUTPUT_INDEX 2
 #define SERVER_OUTPUT_INDEX 3
 
 bool start_read(char *buffer, size_t buffer_size, HANDLE handle, OVERLAPPED *overlapped, HANDLE out_handle) {
@@ -215,19 +210,28 @@ bool create_virtual_console(HPCON *virtual_console, LPPROC_THREAD_ATTRIBUTE_LIST
     return false;
 }
 
-static ProcessEventHandlerArg process_event_handler_arg = {0};
-DWORD WINAPI process_event_handler(void *_arg) {
-    ProcessEventHandlerArg* arg = _arg;
+typedef enum {
+    CONNECTED,
+    CONNECTING,
+    UNCONNECTED
+} PipeState;
 
-    HANDLE process_out = INVALID_HANDLE_VALUE, process_write_out = INVALID_HANDLE_VALUE;
-    OVERLAPPED process_out_overlapped = {0};
-    if (!create_async_connected_named_pipe(POWERSHELL_STDOUT_PIPE_NAME, &process_out, &process_out_overlapped, &process_write_out)) {
+int server_main(void) {
+    HANDLE console_input_read_end = INVALID_HANDLE_VALUE, console_input_write_end = INVALID_HANDLE_VALUE;
+    if (!CreatePipe(&console_input_read_end, &console_input_write_end, NULL, 0)) {
+        nob_log(NOB_ERROR, "Failed to setup console input pipe, %s", win32_error_message(GetLastError()));
+        return 1;
+    }
+
+    HANDLE console_output_read_end = INVALID_HANDLE_VALUE, console_output_write_end = INVALID_HANDLE_VALUE;
+    OVERLAPPED console_output_read_end_overlapped = {0};
+    if (!create_async_connected_named_pipe(POWERSHELL_STDOUT_PIPE_NAME, &console_output_read_end, &console_output_read_end_overlapped, &console_output_write_end)) {
         return 1;
     }
 
     HPCON virtual_console = {0};
     LPPROC_THREAD_ATTRIBUTE_LIST virtual_console_proc_thread_attribute_list = NULL;
-    if (!create_virtual_console(&virtual_console, &virtual_console_proc_thread_attribute_list, process_write_out, arg->thread_in)) {
+    if (!create_virtual_console(&virtual_console, &virtual_console_proc_thread_attribute_list, console_output_write_end, console_input_read_end)) {
         return 1;
     }
 
@@ -254,122 +258,7 @@ DWORD WINAPI process_event_handler(void *_arg) {
         return 1;
     }
 
-    const HANDLE handles[] = {
-        [STDOUT_INDEX] = process_out_overlapped.hEvent,
-        [PROCESS_INDEX] = process_info.hProcess
-        // [PROCESS_THREAD_INDEX] = arg->process.hThread,
-    };
-    char stdout_buffer[PIPE_BUFFER_SIZE] = {0};
-
-    if (!start_read(stdout_buffer, ARRAY_LEN(stdout_buffer), process_out, &process_out_overlapped, arg->thread_out)) {
-        return 1;
-    }
-    bool process_exited = false;
-
-    while (true) {
-        DWORD result = WaitForMultipleObjects(ARRAY_LEN(handles), handles, FALSE, INFINITE);
-        if (result == WAIT_FAILED) {
-            nob_log(NOB_ERROR, "Failed to wait for handle state, %s", win32_error_message(GetLastError()));
-            break;
-        }
-        if (result >= WAIT_ABANDONED_0) {
-            TODO("Handle abandoned");
-            break;
-        }
-
-        int index = result - WAIT_OBJECT_0;
-        if (index < 0 || index >= (int)ARRAY_LEN(handles)) {
-            nob_log(NOB_ERROR, "Index out of range");
-            break;
-        }
-
-        ResetEvent(handles[index]);
-
-        DWORD bytes = 0;
-        if (index == STDOUT_INDEX) {
-            if (!GetOverlappedResult(process_out, &process_out_overlapped, &bytes, FALSE)) {
-                nob_log(NOB_ERROR, "Failed to get result from stdout, %s", win32_error_message(GetLastError()));
-                break;
-            }
-            if (bytes == 0) {
-                break;
-            }
-            if (!WriteFile(arg->thread_out, stdout_buffer, bytes, NULL, NULL)) {
-                nob_log(NOB_ERROR, "Failed to write to thread out, %s", win32_error_message(GetLastError()));
-                break;
-            }
-            if (!start_read(stdout_buffer, ARRAY_LEN(stdout_buffer), process_out, &process_out_overlapped, arg->thread_out)) {
-                break;
-            }
-            continue;
-        }
-
-        if (index == PROCESS_INDEX) {
-            process_exited = true;
-            DWORD exit_code = (DWORD)-1;
-            GetExitCodeProcess(process_info.hProcess, &exit_code);
-            nob_log(NOB_INFO, "Powershell process exited, code: (%d)", exit_code);
-            break;
-        }
-
-        UNREACHABLE("Unknown index");
-    }
-
-    if (!process_exited) {
-        TerminateProcess(process_info.hProcess, 1);
-    }
-    CloseHandle(arg->thread_in);
-    CloseHandle(arg->thread_out);
-    CloseHandle(process_out_overlapped.hEvent);
-    CloseHandle(process_out);
-    CloseHandle(process_info.hProcess);
-    CloseHandle(process_info.hThread);
-    free(virtual_console_proc_thread_attribute_list);
-    ClosePseudoConsole(virtual_console);
-    nob_log(NOB_INFO, "Process event handler thread exited");
-
-    // NOTE: do this to exit on `exit` command ^^, a little hacky but fine for now
-    FreeConsole();
-
-    return 0;
-}
-
-typedef enum {
-    CONNECTED,
-    CONNECTING,
-    UNCONNECTED
-} PipeState;
-
-int server_main(void) {
-    HANDLE thread_input_read_end, thread_input_write_end;
-    if (!CreatePipe(&thread_input_read_end, &thread_input_write_end, NULL, 0)) {
-        nob_log(NOB_ERROR, "Failed to create stderr pipe %s", win32_error_message(GetLastError()));
-        return 1;
-    }
-
-    HANDLE thread_output_read_end = INVALID_HANDLE_VALUE, thread_output_write_end = INVALID_HANDLE_VALUE;
-    OVERLAPPED thread_output_overlapped = {0};
-    if (!create_async_connected_named_pipe(THREAD_OUTPUT_PIPE_NAME, &thread_output_read_end, &thread_output_overlapped, &thread_output_write_end)) {
-        return 1;
-    }
-
-    process_event_handler_arg.thread_in = thread_input_read_end;
-    process_event_handler_arg.thread_out = thread_output_write_end;
-
-    DWORD reader_thread_id = 0;
-    HANDLE thread_handle = CreateThread(
-            NULL,                       // default security attributes
-            0,                          // use default stack size
-            process_event_handler,      // thread function name
-            &process_event_handler_arg, // argument to thread function
-            0,                          // use default creation flags
-            &reader_thread_id);
-    if (thread_handle == INVALID_HANDLE_VALUE) {
-        nob_log(NOB_ERROR, "Failed to create powershell handler thread, %s", win32_error_message(GetLastError()));
-        return 1;
-    }
-
-    nob_log(NOB_INFO, "Powershell handler thread created");
+    nob_log(NOB_INFO, "Powershell process created");
 
     PipeState output_state = UNCONNECTED;
     OVERLAPPED output_write_end_overlapped = {0};
@@ -388,9 +277,10 @@ int server_main(void) {
     HANDLE handles[] = {
         [SERVER_INPUT_INDEX] = input_read_end_overlapped.hEvent,
         [SERVER_OUTPUT_INDEX] = output_write_end_overlapped.hEvent,
-        [PROCESS_HANDLER_THREAD_INDEX] = thread_handle,
-        [THREAD_OUTPUT_INDEX] = thread_output_overlapped.hEvent,
+        [PROCESS_HANDLE_INDEX] = process_info.hProcess,
+        [CONSOLE_OUTPUT_INDEX] = console_output_read_end_overlapped.hEvent,
     };
+
     char input_buffer[PIPE_BUFFER_SIZE] = {0};
     char output_buffer[PIPE_BUFFER_SIZE] = {0};
     size_t unsent_output_buffer_length = 0;
@@ -402,7 +292,7 @@ int server_main(void) {
     bool reset_connection = false;
 
     while (running) {
-        while (output_state == UNCONNECTED && WaitForSingleObject(thread_handle, 10) == WAIT_TIMEOUT) {
+        while (output_state == UNCONNECTED && WaitForSingleObject(process_info.hProcess, 10) == WAIT_TIMEOUT) {
             ConnectNamedPipe(output_write_end, &output_write_end_overlapped);
             DWORD connect_error_code = GetLastError();
             if (connect_error_code == ERROR_PIPE_CONNECTED) {
@@ -420,7 +310,7 @@ int server_main(void) {
             output_state = CONNECTING;
         }
 
-        while (input_state == UNCONNECTED && WaitForSingleObject(thread_handle, 10) == WAIT_TIMEOUT) {
+        while (input_state == UNCONNECTED && WaitForSingleObject(process_info.hProcess, 10) == WAIT_TIMEOUT) {
             ConnectNamedPipe(input_read_end, &input_read_end_overlapped);
             DWORD connect_error_code = GetLastError();
             if (connect_error_code == ERROR_PIPE_CONNECTED) {
@@ -471,12 +361,12 @@ int server_main(void) {
                 } else if (bytes == 0) {
                     reset_connection = true;
                     break;
-                } else if (!WriteFile(thread_input_write_end, input_buffer, bytes*sizeof(*input_buffer), NULL, NULL)) {
-                    nob_log(NOB_ERROR, "Failed to write input to process handler, %s", win32_error_message(GetLastError()));
+                } else if (!WriteFile(console_input_write_end, input_buffer, bytes*sizeof(*input_buffer), NULL, NULL)) {
+                    nob_log(NOB_ERROR, "Failed to write input to process, %s", win32_error_message(GetLastError()));
                     break;
                 }
 
-                if (!start_read(input_buffer, ARRAY_LEN(input_buffer), input_read_end, &input_read_end_overlapped, thread_input_write_end)) {
+                if (!start_read(input_buffer, ARRAY_LEN(input_buffer), input_read_end, &input_read_end_overlapped, console_input_write_end)) {
                     reset_connection = true;
                     break;
                 }
@@ -513,7 +403,7 @@ int server_main(void) {
                     }
 
                     unsent_output_buffer_length = 0;
-                    if (!start_read(output_buffer, ARRAY_LEN(output_buffer), thread_output_read_end, &thread_output_overlapped, output_write_end)) {
+                    if (!start_read(output_buffer, ARRAY_LEN(output_buffer), console_output_read_end, &console_output_read_end_overlapped, output_write_end)) {
                         break;
                     }
 
@@ -521,11 +411,11 @@ int server_main(void) {
                 }
             } break;
 
-            case THREAD_OUTPUT_INDEX: {
+            case CONSOLE_OUTPUT_INDEX: {
                 started_reading_process_output = false;
 
-                if (!GetOverlappedResult(thread_output_read_end, &thread_output_overlapped, &bytes, FALSE)) {
-                    nob_log(NOB_WARNING, "Failed to get process handler output overlapped result, %s", win32_error_message(GetLastError()));
+                if (!GetOverlappedResult(console_output_read_end, &console_output_read_end_overlapped, &bytes, FALSE)) {
+                    nob_log(NOB_WARNING, "Failed to get console output overlapped result, %s", win32_error_message(GetLastError()));
                     break;
                 }
 
@@ -560,15 +450,19 @@ int server_main(void) {
                     unsent_output_buffer_length -= to_be_erased_when_output_full;
                 }
 
-                if (!start_read(output_buffer + unsent_output_buffer_length, ARRAY_LEN(output_buffer) - unsent_output_buffer_length, thread_output_read_end, &thread_output_overlapped, output_write_end)) {
+                if (!start_read(output_buffer + unsent_output_buffer_length,
+                                ARRAY_LEN(output_buffer) - unsent_output_buffer_length,
+                                console_output_read_end,
+                                &console_output_read_end_overlapped,
+                                output_write_end)) {
                     break;
                 }
 
                 started_reading_process_output = true;
             } break;
 
-            case PROCESS_HANDLER_THREAD_INDEX: {
-                nob_log(NOB_INFO, "Process thread handler exited");
+            case PROCESS_HANDLE_INDEX: {
+                nob_log(NOB_INFO, "Process exited");
                 running = false;
             } break;
         }
@@ -583,14 +477,23 @@ int server_main(void) {
         }
     }
 
-    CloseHandle(thread_output_overlapped.hEvent);
-    CloseHandle(thread_output_read_end);
-    CloseHandle(thread_input_write_end);
-    CloseHandle(thread_handle);
-    CloseHandle(input_read_end_overlapped.hEvent);
-    CloseHandle(input_read_end);
     CloseHandle(output_write_end_overlapped.hEvent);
     CloseHandle(output_write_end);
+
+    CloseHandle(input_read_end_overlapped.hEvent);
+    CloseHandle(input_read_end);
+
+    CloseHandle(process_info.hThread);
+    CloseHandle(process_info.hProcess);
+
+    CloseHandle(console_input_write_end);
+    CloseHandle(console_input_read_end);
+    CloseHandle(console_output_write_end);
+    CloseHandle(console_output_read_end_overlapped.hEvent);
+    CloseHandle(console_output_read_end);
+    free(virtual_console_proc_thread_attribute_list);
+    ClosePseudoConsole(virtual_console);
+
     nob_log(NOB_INFO, "Server exited");
 
     return 0;
@@ -831,3 +734,4 @@ int main(int argc, char **argv) {
 // [_] TODO: allow customize shell
 // [X] TODO: client check and start server if needed
 // [x] TODO: log to file for both client and server
+// [x] TODO: remove unnecessary process handler thread in server
